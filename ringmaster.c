53e8c2bce0dd851c0d25a8ace67230dd5c0776ca
Xuan Yang <xy91@duke.edu>
2019-02-10 14:33:23 -0500

    set up game print message & start & end game for ringmaster, not delve into game details

--- ringmaster.c
+++ ringmaster.c
@@ -25,10 +25,14 @@ int main(int argc, const char * argv[]) {
 
   // socket variables
   int master_fd = 0;
-  int player_fd = 0;
+  int *player_fd = malloc(sizeof(*player_fd) * N);
   struct sockaddr_in masterAddr;
-  struct sockaddr_in playerAddr;
+  struct sockaddr_in *playerAddr = malloc(sizeof(*playerAddr) * N);
+  int playerID = 0;
   pid_t pid;
+
+  char buffer[512];
+  socklen_t len;
 
   // Print initial message
   printf("Potato Ringmaster\n");
@@ -47,7 +51,7 @@ int main(int argc, const char * argv[]) {
   memset(&masterAddr, 0, sizeof(masterAddr));
   masterAddr.sin_family = AF_INET;
   masterAddr.sin_port = htons(PORT);
-  masterAddr.sin_addr.s_addr = inet_addr("0.0.0.0");  /* need to change addr */
+  masterAddr.sin_addr.s_addr = inet_addr("127.0.0.1");  /* need to change addr */
 
   // Bind socket to address:port
   if (bind(master_fd, (struct sockaddr*)&masterAddr, sizeof(masterAddr)) < 0) {
@@ -60,6 +64,40 @@ int main(int argc, const char * argv[]) {
     perror("Error in listening.\n");
     exit(1);
   }
+
+  while (1) {
+    if (playerID < N) { // prepare game
+      player_fd[playerID] = accept(master_fd, (struct sockaddr*)&playerAddr[playerID], &len);
+      if (player_fd[playerID] < 0) exit(1);
+      printf("Player %d is ready to play\n", playerID);
+
+      if ((pid = fork()) == 0) { // child
+	close(master_fd);
+
+	// set buffer
+	memset(buffer, '\0', sizeof(buffer));
+	sprintf(buffer, "%d %d ", playerID, N);
+
+	// send
+	send(player_fd[playerID], buffer, 512, 0);
+      }
+      else { // parent
+	playerID++;
+      }
+    }
+    else { // start the game
+      printf("Ready to start the game, sending potato to player X\n");
+      break;
+    }
+  }
+
+  // close & free
+  close(master_fd);
+  for (int i = 0; i < N; i++) {
+      close(player_fd[i]);
+  }
+  free(playerAddr);
+  free(player_fd);
   
   return 0;
 }
