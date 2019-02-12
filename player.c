536afcc8bc46b4166261f0123d104624e9b1955c
Xuan Yang <xy91@duke.edu>
2019-02-11 22:17:51 -0500

    send port to master, but something wrong

--- player.c
+++ player.c
@@ -1,190 +1,205 @@
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
-#include <netdb.h>
-#include <unistd.h>
 #include <sys/types.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
+#include <unistd.h>
 
-int main(int argc, const char * argv[]) {
-    if (argc != 3) {
-        perror("Program invoke format: player <machine_name> <port_num>\n");
-        exit(1);
-    }
+int main(int argc, const char *argv[]) {
+  if (argc != 3) {
+    perror("Program invoke format: player <machine_name> <port_num>\n");
+    exit(1);
+  }
 
-    // Get master ip
-    struct hostent *hent;
-    hent = gethostbyname(argv[1]);
-    if (NULL == hent) {
-        return -1;
-    }
-    char * serIP;
-    serIP = inet_ntoa(*(struct in_addr*)hent->h_addr);
+  // Get master ip
+  struct hostent *hent;
+  hent = gethostbyname(argv[1]);
+  if (NULL == hent) {
+    return -1;
+  }
+  char *serIP;
+  serIP = inet_ntoa(*(struct in_addr *)hent->h_addr);
 
-    // master parameter
-    struct sockaddr_in masterAddr;
-    int PORT = atoi(argv[2]);
+  // master parameter
+  struct sockaddr_in masterAddr;
+  int PORT = atoi(argv[2]);
 
-    // player parameters
-    int count = 0;
-    int player_fd;
-    int ID;
-    int N;
-    char buffer[512];
+  // player parameters
+  int count = 0;
+  int player_fd;
+  int ID;
+  int N;
+  char buffer[512];
 
-    /***********************
-    *        master        *
-    ***********************/
-    // Get master address
-    memset(&masterAddr, 0, sizeof(masterAddr));
-    masterAddr.sin_family = AF_INET;
-    masterAddr.sin_port = htons(PORT);
-    masterAddr.sin_addr.s_addr = inet_addr(serIP); 
+  /***********************
+   *        master        *
+   ***********************/
+  // Get master address
+  memset(&masterAddr, 0, sizeof(masterAddr));
+  masterAddr.sin_family = AF_INET;
+  masterAddr.sin_port = htons(PORT);
+  masterAddr.sin_addr.s_addr = inet_addr(serIP);
 
-    // create socket - self <-> ringmaster
-    player_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (player_fd == -1) {
-        perror("Error: cannot create socket\n");
-        exit(1);
-    } //if
+  // create socket - self <-> ringmaster
+  player_fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (player_fd == -1) {
+    perror("Error: cannot create socket\n");
+    exit(1);
+  } // if
 
-    // connect
-    if (connect(player_fd, (struct sockaddr*)&masterAddr, sizeof(masterAddr)) < 0) {
-        perror("Error: cannot connect to socket\n");
-        exit(1);
-    }
+  // connect
+  if (connect(player_fd, (struct sockaddr *)&masterAddr, sizeof(masterAddr)) <
+      0) {
+    perror("Error: cannot connect to socket\n");
+    exit(1);
+  }
 
+  // neighbor parameters
+  struct sockaddr_in selfAddr;
+  int left_fd;
+  int PORTL = PORT;
+  struct sockaddr_in leftAddr; // for sending
+  int right_r_fd;
+  int right_s_fd;
+  int PORTR = PORT;
+  struct sockaddr_in rightAddr; // for sending
+  socklen_t len;
 
+  /***********************
+   *         left         *
+   ***********************/
+  left_fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (left_fd == -1) {
+    perror("Error: cannot create socket\n");
+    exit(1);
+  } // if
 
+  /************************
+   *        right         *
+   ***********************/
+  right_r_fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (right_r_fd == -1) {
+    perror("Error: cannot create socket\n");
+    exit(1);
+  } // if
 
-    // neighbor parameters
-    struct sockaddr_in selfAddr; 
-    int left_fd;
-    struct sockaddr_in leftAddr; // for sending
-    int right_r_fd;
-    int right_s_fd;
-    struct sockaddr_in rightAddr; // for sending
-    socklen_t len;
+  // Get self ip
+  char selfname[128];
+  if (gethostname(selfname, sizeof(selfname)) == -1) {
+    return -1;
+  }
+  struct hostent *selfhent;
+  selfhent = gethostbyname(selfname);
+  if (selfhent == NULL) {
+    return -1;
+  }
+  char *selfIP;
+  // get from h_addr
+  selfIP = inet_ntoa(*(struct in_addr *)selfhent->h_addr);
 
-    /***********************
-    *         left         *
-    ***********************/
-    left_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (left_fd == -1) {
-        perror("Error: cannot create socket\n");
-        exit(1);
-    } //if
+  // Initial memory address
+  memset(&selfAddr, '\0', sizeof(selfAddr));
+  selfAddr.sin_family = AF_INET;
+  selfAddr.sin_port = htons(PORTR);
+  selfAddr.sin_addr.s_addr = inet_addr(selfIP);
 
-
-
-   /************************
-    *        right         *
-    ***********************/
-    right_r_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (right_r_fd == -1) {
-        perror("Error: cannot create socket\n");
-        exit(1);
-    } //if
-
-    // Get self ip
-    char selfname[128];
-    if (gethostname(selfname, sizeof(selfname)) == -1) {
-        return -1;
-    }
-    struct hostent *selfhent;
-    selfhent = gethostbyname(selfname);
-    if (selfhent == NULL) {
-        return -1;
-    }
-    char * selfIP;
-    // get from h_addr
-    selfIP = inet_ntoa(*(struct in_addr*)selfhent->h_addr);
-
-    // Initial memory address
-    memset(&selfAddr, '\0', sizeof(selfAddr));
+  // bind while cannot succeed
+  while (bind(right_r_fd, (struct sockaddr *)&selfAddr, sizeof(selfAddr)) < 0) {
+    PORTR = PORTR == 65535 ? 0 : PORTR + 1;
+    selfAddr.sin_port = htons(PORTR);
     selfAddr.sin_family = AF_INET;
-    selfAddr.sin_port = htons(PORT);
-    selfAddr.sin_addr.s_addr = inet_addr(selfIP); 
+    selfAddr.sin_addr.s_addr = inet_addr(selfIP);
+  }
 
-    // bind
-    if (bind(right_r_fd, (struct sockaddr*)&selfAddr, sizeof(selfAddr)) < 0) {
-        perror("Error in binding right socket.\n");
-        exit(1);    
-    }
+  // Listening
+  if (listen(right_r_fd, 1) != 0) { /* need to change N? */
+    perror("Error in listening right.\n");
+    exit(1);
+  }
 
-    // Listening
-    if (listen(right_r_fd, 1) != 0) {  /* need to change N? */
-        perror("Error in listening right.\n");
-        exit(1);
-    }
+  // accept
+  right_s_fd = accept(right_r_fd, (struct sockaddr *)&selfAddr, &len);
+  if (right_s_fd < 0) {
+    perror("right accept fails\n");
+    exit(1);
+  }
 
-    // accept
-    right_s_fd = accept(right_r_fd, (struct sockaddr*)&selfAddr, &len);
-    if (right_s_fd < 0) {
-        perror("right accept fails\n");
-        exit(1);
-    }
+  while (1) {
+    if (count == 0) {
+      if (recv(player_fd, buffer, 512, 0) <
+          0) // first recv: "ID N " - used for get ID and N
+        printf("Error in receiving data 0.\n");
+      else {
+        /*****************************************
+         *     receive info & send port number    *
+         *****************************************/
+        // get info
+        char *id = strtok(buffer, " ");
+        char *n = strtok(NULL, " ");
+        ID = atoi(id);
+        N = atoi(n);
 
+        // print status
+        printf("Connected as player %s out of %s total players\n", id, n);
 
+        // set buffer
+        memset(buffer, '\0', sizeof(buffer));
+        sprintf(buffer, "%d", PORTR);
 
+        // send port number
+        send(player_fd, buffer, 512, 0);
 
-    while (1) {
-        if (count == 0) {
-            if (recv(player_fd, buffer, 512, 0) < 0)  // first recv: "ID N " - used for get ID and N
-                printf("Error in receiving data 0.\n");
-            else {
-                // get info
-                char *id = strtok(buffer, " ");
-                char *n = strtok(NULL, " ");
-                ID = atoi(id);
-                N = atoi(n);
+        // reset buffer & update count
+        memset(buffer, '\0', sizeof(buffer));
+        count++;
+      }
+    } else if (count == 1) {
+      if (recv(player_fd, buffer, 512, 0) <
+          0) { // second recv: "leftIP leftPORT " - get address for neighbor and
+               // build connection
+        printf("Error in receiving data 1.\n");
+      } else {
+        /***************************************************
+         * left socket sends connection, right socket waits *
+         ***************************************************/
 
-                // print status
-                printf("Connected as player %s out of %s total players\n", id, n);
-                
-                // reset buffer & update count
-                memset(buffer, '\0', sizeof(buffer));
-                count++;
-            }
-        } else if (count == 1) {
-            if (recv(player_fd, buffer, 512, 0) < 0) { // second recv: "leftIP " - get address for neighbor and build connection
-                printf("Error in receiving data 1.\n");
-            }
-            else {
-                /***************************************************
-                * left socket sends connection, right socket waits *
-                ***************************************************/
+        char *leftIP = strtok(buffer, " ");
+        char *leftPORT = strtok(NULL, " ");
+        PORTL = atoi(leftPORT);
 
-                // step1: get IP from buffer
-                memset(&leftAddr, 0, sizeof(leftAddr));
-                leftAddr.sin_family = AF_INET;
-                leftAddr.sin_port = htons(PORT);
-                leftAddr.sin_addr.s_addr = inet_addr(buffer); 
+        // step1: get IP from buffer
+        memset(&leftAddr, 0, sizeof(leftAddr));
+        leftAddr.sin_family = AF_INET;
+        leftAddr.sin_port = htons(PORTL);
+        leftAddr.sin_addr.s_addr = inet_addr(leftIP);
 
-                // step2: connect
-                if (connect(left_fd, (struct sockaddr*)&leftAddr, sizeof(leftAddr)) < 0) {
-                    perror("Error: cannot connect to left socket\n");
-                    exit(1);
-                }
-
-                // reset buffer & update count
-                memset(buffer, '\0', sizeof(buffer));
-                count++;
-            }
-        } else if (count == 2) {
-            if (recv(player_fd, buffer, 512, 0) < 0) {  // third recv: "hops " - get hops from server
-                printf("Error in receiving data 2.\n");
-            }
-            else {
-
-            }
+        // step2: connect
+        if (connect(left_fd, (struct sockaddr *)&leftAddr, sizeof(leftAddr)) <
+            0) {
+          perror("Error: cannot connect to left socket\n");
+          exit(1);
         }
 
-        // close(player_fd);
-        // printf("Disconnect\n");
-        // exit(1);
+        printf("Connect player %d's left socket to %s\n", ID, leftIP);
+
+        // reset buffer & update count
+        memset(buffer, '\0', sizeof(buffer));
+        count++;
+      }
+    } else if (count == 2) {
+      if (recv(player_fd, buffer, 512, 0) <
+          0) { // third recv: "hops " - get hops from server
+        printf("Error in receiving data 2.\n");
+      } else {
+      }
     }
-    return 0;
+
+    // close(player_fd);
+    // printf("Disconnect\n");
+    // exit(1);
+  }
+  return 0;
 }
--- ringmaster.c
+++ ringmaster.c
@@ -1,127 +1,152 @@
+#include <arpa/inet.h>
+#include <netdb.h> // gethostbyname
+#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
-#include <netdb.h>  // gethostbyname
-#include <unistd.h>  
 #include <sys/types.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>  
+#include <unistd.h>
 
+int main(int argc, const char *argv[]) {
+  // Get input
+  if (argc != 4) {
+    perror("Program invoke format: ringmaster <port_num> <num_players> "
+           "<num_hops>\n");
+    exit(1);
+  }
 
-int main(int argc, const char * argv[]) {
-    // Get input
-    if (argc != 4) {
-        perror("Program invoke format: ringmaster <port_num> <num_players> <num_hops>\n");
+  int PORT = atoi(argv[1]);
+  int N = atoi(argv[2]);
+  int hops = atoi(argv[3]);
+
+  if (N < 1 || hops < 0 || hops > 512) {
+    perror("Invalid num_players or num_hops\n");
+    exit(1);
+  }
+
+  // socket variables
+  int master_fd, ret;
+  struct sockaddr_in masterAddr;
+
+  int playerID = 0;
+  int *player_fd = malloc(sizeof(*player_fd) * N); // newSocket
+  int *player_PORT = malloc(sizeof(*player_PORT) * N);
+  struct sockaddr_in *playerAddr = malloc(sizeof(*playerAddr) * N); // newAddr
+
+  socklen_t len;
+
+  char buffer[512];
+  pid_t childpid;
+  int count = 0;
+
+  // Print initial message
+  printf("Potato Ringmaster\n");
+  printf("Players = %d\n", N);
+  printf("Hops = %d\n", hops);
+
+  // Create ringmaster socket
+  master_fd = socket(AF_INET, SOCK_STREAM, 0); /* need to change AF_INET? */
+  // error message
+  if (master_fd < 0) {
+    perror("[-]Error in connection.\n");
+    exit(1);
+  }
+
+  // Get ip
+  char hostname[128];
+  if (gethostname(hostname, sizeof(hostname)) == -1) {
+    return -1;
+  }
+  struct hostent *hent;
+  hent = gethostbyname(hostname);
+  if (NULL == hent) {
+    return -1;
+  }
+
+  char *serIP;
+  // get from h_addr
+  serIP = inet_ntoa(*(struct in_addr *)hent->h_addr);
+
+  // Initial memory address
+  memset(&masterAddr, '\0', sizeof(masterAddr));
+  masterAddr.sin_family = AF_INET;
+  masterAddr.sin_port = htons(PORT);
+  masterAddr.sin_addr.s_addr = inet_addr(serIP);
+
+  // Bind socket to address:port
+  ret = bind(master_fd, (struct sockaddr *)&masterAddr, sizeof(masterAddr));
+  // error message
+  if (ret < 0) {
+    perror("[-]Error in binding.\n");
+    exit(1);
+  }
+
+  // Listening
+  if (listen(master_fd, N) != 0) { /* need to change N? */
+    perror("[-]Error in listening.\n");
+    exit(1);
+  }
+
+  while (1) {
+    if (playerID < N) { // prepare game
+      player_fd[playerID] =
+          accept(master_fd, (struct sockaddr *)&playerAddr[playerID], &len);
+      if (player_fd[playerID] < 0) {
         exit(1);
-    }
-    
-    int PORT = atoi(argv[1]);
-    int N = atoi(argv[2]);
-    int hops = atoi(argv[3]);
+      }
+      printf("Player %d is ready to play\n", playerID);
+      printf("Accept from %s\n", inet_ntoa(playerAddr[playerID].sin_addr));
 
+      // set buffer
+      memset(buffer, '\0', sizeof(buffer));
+      sprintf(buffer, "%d %d ", playerID, N);
 
-    if (N < 1 || hops < 0 || hops > 512) {
-        perror("Invalid num_players or num_hops\n");
-        exit(1);
-    }
+      // send ID
+      send(player_fd[playerID], buffer, 512, 0);
 
-    // socket variables
-    int master_fd, ret;
-    struct sockaddr_in masterAddr;
+      // receive PORT
+      memset(buffer, '\0', sizeof(buffer));
+      recv(player_fd[playerID], buffer, 512, 0);
+      player_PORT[playerID] = atoi(buffer);
 
-    int playerID = 0;
-    int *player_fd = malloc(sizeof(*player_fd) * N); // newSocket
-    struct sockaddr_in *playerAddr = malloc(sizeof(*playerAddr) * N); // newAddr
+      // reset buffer & increase ID
+      memset(buffer, '\0', sizeof(buffer));
+      playerID++;
+    } else { // start game
+      if (count ==
+          0) { // step1: send address of left neighbor - "leftAddr leftPORT "
+        printf("Ready to start the game, set's get some address done\n");
 
-    socklen_t len; 
+        for (int i = 0; i < N; i++) {
+          int left = i == 0 ? N - 1 : i - 1;
 
-    char buffer[1024];
-    pid_t childpid;
+          // set buffer
+          memset(buffer, '\0', sizeof(buffer));
+          sprintf(buffer, "%s %d ", inet_ntoa(playerAddr[left].sin_addr),
+                  player_PORT[left]);
 
-    // Print initial message
-    printf("Potato Ringmaster\n");
-    printf("Players = %d\n", N);
-    printf("Hops = %d\n", hops);
-
-    // Create ringmaster socket
-    master_fd = socket(AF_INET, SOCK_STREAM, 0); /* need to change AF_INET? */
-    // error message
-    if (master_fd < 0) {
-        perror("[-]Error in connection.\n");
-        exit(1);        
-    }
-
-    // Get ip
-    char hostname[128];
-    if (gethostname(hostname, sizeof(hostname)) == -1) {
-        return -1;
-    }
-    struct hostent *hent;
-    hent = gethostbyname(hostname);
-    if (NULL == hent) {
-        return -1;
-    }
-
-    char * serIP;
-    // get from h_addr
-    serIP = inet_ntoa(*(struct in_addr*)hent->h_addr);
-
-    // Initial memory address
-    memset(&masterAddr, '\0', sizeof(masterAddr));
-    masterAddr.sin_family = AF_INET;
-    masterAddr.sin_port = htons(PORT);
-    masterAddr.sin_addr.s_addr = inet_addr(serIP); // modify later
-
-    // Bind socket to address:port
-    ret = bind(master_fd, (struct sockaddr*)&masterAddr, sizeof(masterAddr));
-    // error message
-    if (ret < 0) {
-        perror("[-]Error in binding.\n");
-        exit(1);        
-    }
-
-    // Listening
-    if (listen(master_fd, N) != 0) {  /* need to change N? */
-        perror("[-]Error in listening.\n");
-        exit(1);
-    }
-
-    while(1) {
-        if (playerID < N) { // prepare game
-            player_fd[playerID] = accept(master_fd, (struct sockaddr*)&playerAddr[playerID], &len);
-            if (player_fd[playerID] < 0) {
-                exit(1);
-            }
-            printf("Player %d is ready to play\n", playerID);
-            printf("Accept from %s\n", inet_ntoa(playerAddr[playerID].sin_addr));
-
-            if ((childpid = fork()) == 0) { // child
-                close(master_fd);
-
-                // set buffer
-                memset(buffer, '\0', sizeof(buffer));
-                sprintf(buffer, "%d %d ", playerID, N);
-
-                // send
-                send(player_fd[playerID], buffer, 1024, 0);
-            } else { // parent
-                playerID++;
-            }
+          printf("Sending No.%d player the address of its left player No.%d: "
+                 "%s - PORT: %d\n",
+                 i, left, inet_ntoa(playerAddr[left].sin_addr),
+                 player_PORT[left]);
+          send(player_fd[i], buffer, 512, 0);
         }
-        else { // start game
-            printf("Ready to start the game, sending potato to player 0\n");
 
-            break;
-        }
+        count++;
+      } else if (count == 1) { // step2: throw the potato
+        printf("I'm going to throw potato now\n");
+      }
     }
+  }
 
-    close(master_fd);
-    for (int i = 0; i < N; i++) {
-        close(player_fd[i]);
-    }
-    free(playerAddr);
-    free(player_fd);
+  close(master_fd);
+  for (int i = 0; i < N; i++) {
+    close(player_fd[i]);
+  }
+  free(playerAddr);
+  free(player_PORT);
+  free(player_fd);
 
-    return 0;
+  return 0;
 }
